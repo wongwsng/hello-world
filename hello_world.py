b9ad61e7186d919f73048f52ba6501d872c7cfbd
xwp <xwp_fullstack@163.com>
2018-06-24 15:41:35 +0800

    The first 'hello world'

---
+++ hello_world.py
@@ -0,0 +1,20 @@
+
+
+def add(num1, num2):
+	return num1 + num2
+
+
+def main():
+	
+	b = 2
+	c = 3
+	d = add(b, c)
+	print(d)
+	
+	a = "hello world"
+	print(a)
+
+if __name__ == '__main__':
+	main()
+
+
